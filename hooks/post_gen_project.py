c70527f2895e0c1ffa9fdf56e1a7486e46c4f93c
Jordane Gengo <jordane.gengo@wolt.com>
2026-02-01 10:29:14 +0200

    fix: remove redundant EOL

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,6 @@
 import shutil
 import subprocess
 
-EOL = "\x1b[0m"
 ERROR = "[\x1b[1;31mERROR\x1b[0m]"
 SUCCESS = "[\x1b[1;32mOK\x1b[0m]"
 INFO = "[\x1b[1;33mINFO\x1b[0m]"
@@ -10,17 +9,17 @@ INFO = "[\x1b[1;33mINFO\x1b[0m]"
 def run_cmd(cmd: list[str], description: str) -> bool:
     try:
         subprocess.run(cmd, capture_output=True, check=True)
-        print(f"{SUCCESS} {description}{EOL}")
+        print(f"{SUCCESS} {description}")
         return True
     except (FileNotFoundError, subprocess.CalledProcessError):
-        print(f"{ERROR} {description} failed{EOL}")
+        print(f"{ERROR} {description} failed")
         return False
 
 
-print(f"{SUCCESS} Sucesfully created project{EOL}")
+print(f"{SUCCESS} Sucesfully created {{cookiecutter.repo_dir}} directory")
 
 shutil.copy("env.sample", ".env")
-print(f"{SUCCESS} Copied env.sample to .env{EOL}")
+print(f"{SUCCESS} Copied env.sample to .env")
 
 run_cmd(["git", "init"], "Initialized git repository")
 print(f"{INFO} Installing dependencies, wait a moment...")
@@ -29,6 +28,6 @@ run_cmd(["poetry", "run", "pre-commit", "install"], "Installed pre-commit hooks"
 run_cmd(["poetry", "run", "black", "."], "Formatted with black")
 run_cmd(["poetry", "run", "isort", "."], "Formatted with isort")
 run_cmd(["git", "add", "."], "Staged files for commit")
-print(f"{INFO} Commiting files, might take a moment because of the pre-commit hooks...{EOL}")
+print(f"{INFO} Commiting files, might take a moment because of the pre-commit hooks...")
 run_cmd(["git", "commit", "-m", "Initial commit"], "Committed files")
-print(f"{SUCCESS} Succesfully created project, now you can start developing!{EOL}")
+print(f"{SUCCESS} Succesfully created project, now you can start developing!")
